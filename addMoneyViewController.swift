a84f3f7bccb3caa69c40ac62ab00b1af742d8b2f
Kyoseung Koo <im@kyo.sk>
2014-08-09 21:54:34 +0900

    add sqlite3 for project

--- addMoneyViewController.swift
+++ addMoneyViewController.swift
@@ -19,9 +19,10 @@ class addMoneyViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
-
+    
     /*
     // MARK: - Navigation
 
---
+++ recipe/sqlite3Bridge.h
@@ -0,0 +1,15 @@
+//
+//  sqlite3Bridge.h
+//  recipe
+//
+//  Created by Kyoseung on 8/9/14.
+//  Copyright (c) 2014 devfun. All rights reserved.
+//
+
+#ifndef recipe_sqlite3Bridge_h
+#define recipe_sqlite3Bridge_h
+
+#include <sqlite3.h>
+
+
+#endif
