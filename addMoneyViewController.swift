ab1d8fa643e3d640cc6a49df592a0d90db7b6f2a
Kyoseung Koo <im@kyo.sk>
2014-08-09 21:28:33 +0900

    connect view with .swift

---
+++ addMoneyViewController.swift
@@ -0,0 +1,35 @@
+//
+//  addMoneyViewController.swift
+//  recipe
+//
+//  Created by Kyoseung on 8/9/14.
+//  Copyright (c) 2014 devfun. All rights reserved.
+//
+
+import UIKit
+
+class addMoneyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
